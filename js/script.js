c0c683f7adb46c2b90fb9635ac3ab327541053d4
MYKim <amnos135@gmail.com>
2022-12-29 23:33:06 +0900

    notice_line

--- js/script.js
+++ js/script.js
@@ -76,6 +76,74 @@ window.console.log.apply(console, Hello);// 콘솔
 
 
 
+$(function(){
+  //보도자료 슬라이드, 위로
+  var current1 = 0;
+  var subtext = $('.sub_text>li');
+  var tim;
+
+  function set(){
+      tim = setInterval(function(){
+          var prev1 = subtext.eq(current1);
+
+          move1(prev1,0,'-100%');
+          current1++;
+          if(current1 == subtext.size()){
+              current1 = 0;
+          }
+          var next1 = subtext.eq(current1);
+          move1(next1, '100%',0);
+          
+      },1500);
+  }
+  set();
+
+  $('.sub_text').hover(function(){
+      clearInterval(tim);
+  },function(){
+      set();
+  });
+
+  function move1(tgg, start, end){
+      tgg.css('top', start).stop().animate({top:end},800);
+  }
+
+  //사회공헌 슬라이드, 위에서 아래로
+  var current2 = 0;
+  var subtext2 = $('.sub_text1>li');
+  var tim2;
+
+  function set2(){
+      tim2 = setInterval(function(){
+          var prev2 = subtext2.eq(current2);
+
+          move2(prev2,0,'100%');
+          current2++;
+          if(current2 == subtext2.size()){
+              current2 = 0;
+          }
+          var next2 = subtext2.eq(current2);
+          move2(next2, '-100%',0);
+          
+      },1500);
+  }
+  set2();
+
+  $('.sub_text1').hover(function(){
+      clearInterval(tim2);
+  },function(){
+      set2();
+  });
+
+  function move2(tgg2, start, end){
+      tgg2.css('top', start).stop().animate({top:end},800);
+  }
+
+
+});
+
+
+
 
 
 
